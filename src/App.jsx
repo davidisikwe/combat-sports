01c409064e8620af21d06cea1fad05fa2b61990e
David Isikwe <disikwe@gmail.com>
2024-05-30 22:34:59 -0500

    about to move to a differnt folder locally

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Navbar from './components/Navbar';
+import Post from './components/Post';
 
 function App() {
   return (
     <div className=''>
       {/* <Login /> */}
       {/* <Signup /> */}
-      <Navbar />
+      {/* <Navbar /> */}
+      <Post />
     </div>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div className='w-[36.375rem] min-h-[20rem] bg-[#FEF7FF] rounded-xl border border-[#CAC4D0]'></div>
+  );
+};
+
+export default Post;
